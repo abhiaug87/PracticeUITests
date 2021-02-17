2713b9327b8d54216e2e654bc0247a9bea1e56fc
XEROINSIDE\abhishek.kulkarni <abhishek.kulkarni@xero.com>
2021-02-17 15:29:52 +1300

    Code to run the tests in headless mode

--- PracticeUITests/Utilities/DriverFactory.cs
+++ PracticeUITests/Utilities/DriverFactory.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Collections.Generic;
 
 namespace PracticeUITests.Utilities
 {
@@ -8,10 +9,18 @@ namespace PracticeUITests.Utilities
     {
         public static IWebDriver InitiateWebDriver(string browser)
         {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--window-size=1920,1080");
+            chromeOptions.AddArguments("--allow-insecure-localhost");
+            chromeOptions.AddArguments("--headless");
+            chromeOptions.AddArguments("--disable-gpu");
+            chromeOptions.AddArguments("--no-sandbox");
             IWebDriver driver = null;
             if (browser.Equals(CommonConstants.DriverSettings.ChromeBrowser))
             {
-                driver = new ChromeDriver(CommonConstants.DriverSettings.BinaryLocationChrome);
+               // driver = new ChromeDriver(CommonConstants.DriverSettings.BinaryLocationChrome);
+                 driver = new ChromeDriver(CommonConstants.DriverSettings.BinaryLocationChrome, chromeOptions);
+
             }
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(CommonConstants.DriverSettings.DefaultWaitTime);
